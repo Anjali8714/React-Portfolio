e49ec71dd73f164dbaebea22201f4dcb219a6f04
Anjali8714 <anjalit9714@gmail.com>
2025-07-24 14:28:42 +0530

    contacts1

--- src/Components/Contacts.jsx
+++ src/Components/Contacts.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { MdAddIcCall } from "react-icons/md";
 import { MdOutlineMail } from "react-icons/md";
+import { FaLinkedin } from "react-icons/fa6";
 
 
 const Contacts = () => {
   return (
-    <section id='contact' className='flex items-center flex-col justify-center p-6 md:p-6'>
-      <h1 className='text-white text-3xl flex justify-center font-bold p-8'>CONTACT ME</h1>
-      <div className='w-[60%] h-100 bg-white/70 rounded-lg flex justify-center items-center flex-col gap-10 text-2xl font-bold'>
-      <div className='flex flex-row gap-10'>
-      <div>
-         <h1><MdAddIcCall />+91 8714369714</h1>
+    <section id='contact' className='flex items-center flex-col justify-center px-4 py-10'>
+      <h1 className='text-white text-3xl font-bold p-8'>CONTACT ME</h1>
+      <div className='w-full max-w-4xl h-100 bg-white/70 rounded-lg p-8 flex items-center flex-col gap-10 text-lg md:text-2xl font-bold'>
+      <div className='flex flex-col md:flex-row justify-between w-full gap-8'>
+      <div className='flex items-center gap-3'><MdAddIcCall className='mt-4'/><span>+91 8714369714</span></div >
+      <div className='flex items-center gap-3'>
+      <MdOutlineMail /> <span>anjaliofficial8714@gmail.com</span> 
+      </div>
+      <div className='flex items-center gap-3'>
+      <MdOutlineMail /> <span> anjalit9714@gmail.com</span>
+      </div>
       </div>
       <div>
-         <h1> <MdOutlineMail /> anjaliofficial8714@gmail.com</h1>
-         <h1>anjalit9714@gmail.com</h1>
+         <FaLinkedin /><a href='https://www.linkedin.com/in/anjali-t-b92748311/' className='hover:underline'>https://www.linkedin.com/in/anjali-t-b92748311/</a>
       </div>
-      </div>
-         <h1>https://www.linkedin.com/in/anjali-t-b92748311/</h1>
       </div>
     </section>
   );
